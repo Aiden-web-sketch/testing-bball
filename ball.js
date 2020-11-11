619684606d9fe629182520d3a2c597627cd1573d
Aiden-web-sketch <72841441+Aiden-web-sketch@users.noreply.github.com>
2020-11-11 13:38:57 -0500

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,24 +1,26 @@
+
+
 class ball {
-    constructor(x, y, width, height) {
+    constructor(x, y,radius) {
       var options = {
-         'isStatic':true,
-          'restitution':0.8,
-          'friction':0.3,
-          'density':1.0
+         'isStatic':false,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
       }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius= radius;
+
       
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
+      push();
       translate(pos.x, pos.y);
-      rectMode(CENTER);
       fill(100,100,150);
-      stroke("black")
-      rect(0, 0, this.width, this.height);
+      ellipseMode(RADIUS);
+      circle(0, 0,this.radius );
+      pop();
     }
   };
-
