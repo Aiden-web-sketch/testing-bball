0793aa60500ae346999c0274d2e87dc9bb984009
Aiden-web-sketch <72841441+Aiden-web-sketch@users.noreply.github.com>
2020-11-11 13:38:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,58 +1,71 @@
 
+
+
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 
-Engine.update(engine);
+
+
+
+
+
+
+
+
+
 function preload()
 {
 
 }
 
 function setup() {
-	createCanvas(700, 600);
+	createCanvas(1600, 650);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-ground = Bodies.rectangle(200,390,400,20);
-
-
+ground = Bodies.rectangle(800,560,1600,20,{isStatic:true})
 
 World.add(world,ground);
  
 
-ball1=new ball(300,490,50,50)
+ball1=new ball(300,490,20)
 
-trash1=new trash(350,80,10,50)
-trash2=new trash(450,70,20,50)
-
+trash1=new trash(750,500,20,80)
+trash2=new trash(900,540,300,20)
+trash3=new trash(1050,500,20,80)
 
 
   Engine.run(engine);
-  
-
-
-
 
   }
-  
-
-
 
 function draw() {
   background("gray");
-
+  rectMode(CENTER);
+rect(ground.position.x,ground.position.y,1600,20)
  ball1.display();
+
  trash1.display();
  trash2.display();
+ trash3.display();
   
-  drawSprites();
+  //drawSprites();
+
+
+  
  
 }
 
-
-
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    
+    
+    Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+  }
+}
